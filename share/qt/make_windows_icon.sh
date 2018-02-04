6350cd588685408ca89661fea8f397a5214eee11
ElDiabloBlanco <36115750+ElDiabloBlanco@users.noreply.github.com>
2018-02-04 05:09:32 -0500

    Update make_windows_icon.sh

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -3,3 +3,4 @@
 ICON_DST=../../src/qt/res/icons/novacoin.ico
 
 convert ../../src/qt/res/icons/novacoin-16.png ../../src/qt/res/icons/novacoin-32.png ../../src/qt/res/icons/novacoin-48.png ${ICON_DST}
+ 
